3d94a1ccfb5b7f12577be196cb6aa4bbaf33530e
TheAbyssalOne <gax1994@hotmail.com>
2024-08-19 20:15:46 +0200

    added count functionalities

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-
+    # print(text)
+    count_words(text)
+    # print(count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_words(list_words):
+    wrds = list_words.split()
+    count = 0
+    for x in wrds:
+        count += 1
+    print(count)
 
 main()
